a366504719661dd48ed0343bce14ddeb954467b3
Loremuel Gadrinab <loremuelgadrinab@gmail.com>
2017-05-23 20:17:16 +0800

    ss

--- TeHelper.php
+++ TeHelper.php
@@ -47,16 +47,19 @@ class TeHelper
 
         $difference = $due_time->diffInHours($created_at);
 
-
-        if($difference <= 90)
-            $time = $due_time;
-        elseif ($difference <= 24) {
-            $time = $created_at->addMinutes(90);
-        } elseif ($difference > 24 && $difference <= 72) {
-            $time = $created_at->addHours(16);
-        } else {
-            $time = $due_time->subHours(48);
-        }
+        switch ($difference) {
+        	case $difference <= 24:
+		        $time = $created_at->addMinutes(90);
+		        break;
+		    case $difference > 24 && $difference <= 72:
+		         $time = $created_at->addHours(16);
+		        break;
+		    case $difference <= 90:
+		        $time = $due_time;
+		        break;  
+		    default:
+		        $time = $due_time->subHours(48);
+		}
 
         return $time->format('Y-m-d H:i:s');
 
